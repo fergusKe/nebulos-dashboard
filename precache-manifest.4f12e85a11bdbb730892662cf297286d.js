8151e86284b63f8e6462b57830b216af54fb16cd
fergus <fergus.jingtai@gmail.com>
2020-07-27 17:39:24 +0800

    Updates

---
+++ precache-manifest.4f12e85a11bdbb730892662cf297286d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a151dcabd83b7a9c4d5b16937105bce2",
+    "url": "/nebulos-dashboard/index.html"
+  },
+  {
+    "revision": "38be425bf7013f61eb7b",
+    "url": "/nebulos-dashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ca00f7216db9d788500",
+    "url": "/nebulos-dashboard/static/js/2.5fe823ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nebulos-dashboard/static/js/2.5fe823ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38be425bf7013f61eb7b",
+    "url": "/nebulos-dashboard/static/js/main.9dcd9fe2.chunk.js"
+  },
+  {
+    "revision": "3984d2436ec325e67359",
+    "url": "/nebulos-dashboard/static/js/runtime-main.e32eff6f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nebulos-dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
